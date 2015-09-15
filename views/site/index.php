46768cb4b3eaf6cc090b5e27f9a05d9f05ef259b
System Administrator <root@e252-248.eduroam.tuwien.ac.at>
2015-09-15 16:48:15 +0200

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -9,17 +9,38 @@ $this->title = 'Crowdfunding';
         <div class="hero-content-holder text-center">
             <h1>Mach deine Stadt grüner!</h1>
             <p class="intro">Wir helfen dir ökologische, nachhaltige und grüne Projekte in deiner Nachbarschaft umzusetzen</p>
-            <a href="#" class="btn btn-fill btn-large">Starte dein Projekt</a>
+            <div class="hero-content-how-it-works">
+                <div class="row">
+                    <div class="col-md-4 intro-feature">
+                        <div>
+                            <div class="intro_step step-one center-block"></div>
+                            <h5>Teile deine Idee</h5>
+                            <p>Lege ein Projekt und wir helfen dir dabei es bekannt zu machen.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 intro-feature">
+                        <div>
+                            <div class="intro_step step-two center-block"></div>
+                            <h5>Hol dir unterstützung</h5>
+                            <p>Finde Leute die dir bei der Umsetzung helfen möchten und lernt euch kennen.
+                        </div>
+                    </div>
+                    <div class="col-md-4 intro-feature">
+                        <div>
+                            <div class="intro_step step-three center-block"></div>
+                            <h5>Setze es um</h5>
+                            <p>Setzt euch Ziele und verwirklicht euer Projekt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
     <div class="hero-image"></div>
 </section>
 
-<section>
-    
-</section>
-
-<section>
+<section class="main-section">
     <div class="container-elements clearfix">
         <div class="pull-left"><h3>Beliebte Projekte</h3></div>
         <div class="pull-right"><a href="#" class="btn btn-medium">Alle Projekte</a></div>
