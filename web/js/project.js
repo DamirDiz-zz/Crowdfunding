38284187da0f95934a297461743ec364a350c6b0
System Administrator <root@ip149-148-6-31.x8021.meduniwien.ac.at>
2015-09-20 17:31:34 +0200

    fixes

--- web/js/project.js
+++ web/js/project.js
@@ -5,18 +5,19 @@ var Project = {
     },
     
     initMap: function() {
-        console.log("init dat hoe");
-        
         var title = $('#map').attr('location');
         var latitude = $('#map').attr('lat');
         var longitude = $('#map').attr('lng');
         var location = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
         
+        
         var map = new google.maps.Map(document.getElementById('map'), {
                 center: location,
                 scrollwheel: false,
-                zoom: 16
-            });
+                zoom: 16,
+                disableDefaultUI: true,
+                styles: [{"featureType":"landscape","stylers":[{"hue":"#FFA800"},{"saturation":0},{"lightness":0},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#53FF00"},{"saturation":-73},{"lightness":40},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FBFF00"},{"saturation":0},{"lightness":0},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#00FFFD"},{"saturation":0},{"lightness":30},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00BFFF"},{"saturation":6},{"lightness":8},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#679714"},{"saturation":33.4},{"lightness":-25.4},{"gamma":1}]}]
+        });
         
         var marker = new google.maps.Marker({
                 position: location,
