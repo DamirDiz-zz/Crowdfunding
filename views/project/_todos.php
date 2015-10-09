675c7a296f88fd6c99bdd8f8f702cf285d994402
System Administrator <root@e254-225.eduroam.tuwien.ac.at>
2015-10-09 14:50:24 +0200

    edit todo

--- views/project/_todos.php
+++ views/project/_todos.php
@@ -3,26 +3,17 @@
 ?>
 
 <div class="todo-list editable">
-    <div class="todo-list-entry" data-todo-id="1" data-todo-content="Hier steht ein nices todo 1">
+    <div class="todo-list-entry" data-todo-id="1" data-todo-content="Hier steht ein nices todo 1" data-todo-edited="0">
         <div class="todo-icon"></div>
         <div class="todo-text"></div>
-        <div class="todo-delete-icon pull-right"></div>
-        <div class="todo-edit-icon pull-right"></div>
+        <div class="todo-action pull-right">
+            <div class="todo-icon-edit"></div>
+            <div class="todo-icon-delete"></div>
+        </div>
     </div>
-    <div class="todo-list-entry" data-todo-id="2" data-todo-content="Hier steht ein nices todo 2">
-        <div class="todo-icon"></div>
-        <div class="todo-text"></div>
-        <div class="todo-delete-icon pull-right"></div>
-        <div class="todo-edit-icon pull-right"></div>
-    </div>
-    <div class="todo-list-entry" data-todo-id="3" data-todo-content="Hier steht ein nices todo 3">
-        <div class="todo-icon"></div>
-        <div class="todo-text"></div>
-        <div class="todo-delete-icon pull-right"></div>
-        <div class="todo-edit-icon pull-right"></div>
-    </div>
+
     <div class="todo-list-add">
-        <div class="todo-add-icon"></div>
-        <div class="todo-add-text">Add Todo</div>
+        <div class="todo-icon-add"></div>
+        <div class="todo-text-add">Add Todo</div>
     </div>
 </div>
--- web/js/Todos.js
+++ web/js/Todos.js
@@ -1,20 +1,49 @@
 $(document).ready(function () {
     
     $('.todo-list-entry').each(function() {
-        $(this).find('.todo-text').text($(this).attr("data-todo-content"));
+        $(this).find('.todo-text').text($(this).data("todo-content"));
     });
     
-    $('.todo-text').on('click', function() {
-        if ($(this).find('input').length === 0) {
-           
-            var text = $(this).parent().attr("data-todo-content");
+    $(document).on("click", ".todo-icon-edit", function() {
+        var button = $(this);
+        var todoobject = button.parent().parent();
+        var editmode = todoobject.data("todo-edited");
+        
+        if (editmode === 0) {
+            todoobject.data("todo-edited", 1);
+            
+            var textdiv = todoobject.find(".todo-text");
+            var todotext = todoobject.data("todo-content");
+            var newInput = $('<textarea>' + todotext + '</textarea>');
+            
+            textdiv.empty();
+            textdiv.append(newInput);
 
-           
-            var newInput = $('<input value="' + text + '">');
-            $(this).empty();
-            $(this).append(newInput);
             newInput.focus();
-        }
+
+            button.removeClass("todo-icon-edit");
+            button.addClass("todo-icon-confirm");
+        } 
+    });
+    
+    $(document).on("click", ".todo-icon-confirm", function() {
+        var button = $(this);
+        var todoobject = button.parent().parent();
+        var editmode = todoobject.data("todo-edited");
+        
+        if (editmode === 1) {
+            todoobject.data("todo-edited", 0);
+            
+            var textdiv = todoobject.find(".todo-text");
+            var inputtext = textdiv.find('textarea').val();
+            todoobject.data("todo-content", inputtext);
+            
+            textdiv.empty();
+            textdiv.text(inputtext);
+
+            button.removeClass("todo-icon-confirm");
+            button.addClass("todo-icon-edit");
+        } 
     });
     
 });
